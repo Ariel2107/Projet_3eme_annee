54bdada96f8b68f62dda9fa18972bb0412c6133c
mourad <siyakoub@gmail.com>
2022-04-29 22:57:00 +0200

    generateur photos d'animaux

--- Datasets/API_Generated_Photos_Animal/API_Generated_Photos_Animal.py
+++ Datasets/API_Generated_Photos_Animal/API_Generated_Photos_Animal.py
@@ -20,24 +20,22 @@ def main():
         for i in range(0, nbIt):
             response = requests.get(url)
             images = response.json()['image_link']
+            image = images.lstrip('htps:/upload.wikimedia.org/wikipedia/commons/.1234567890sbzooinstitutes.com/img/animals/86fineartamerica.com/images-medium-large-5/')
+            print(Fore.RED + image)
+            try:
+                open("images/" + image)
+                print("Le fichier existe deja !")
+            except OSError:
+                print(Fore.GREEN + "1 image enregitré  : " + image)
+                urllib.request.urlretrieve(images, "images/" + image)
+            except HTTPError:
+                time.sleep(60)
+                print(Fore.GREEN + "1 image enregitré !")
+                urllib.request.urlretrieve(images, "images/" + image)
+                urllib.request.urlretrieve(images, "images/" + image)
     except HTTPError:
         print("Pause ... attend tu as envoyé trop de requêtes !")
-        time.sleep(60)
-        response = requests.get(url)
-        images = response.json()['image_link']
-        image = images.lstrip('htps:/upload.wikimedia.org/wikipedia/commons/.1234567890sbzooinstitutes.com/img/animals/86fineartamerica.com/images-medium-large-5/')
-        print(Fore.RED + image)
-    try:
-        open("images/" + image)
-        print("Le fichier existe deja !")
-    except OSError:
-        print(Fore.GREEN + "1 image enregitré  : " + image)
-        urllib.request.urlretrieve(images, "images/" + image)
-    except HTTPError:
-        time.sleep(60)
-        print(Fore.GREEN + "1 image enregitré !")
-        urllib.request.urlretrieve(images, "images/" + image)
-        urllib.request.urlretrieve(images, "images/" + image)
+
 
 if __name__ == '__main__':
     main()
