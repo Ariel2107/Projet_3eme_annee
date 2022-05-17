03f70599fe1a822879542b3b1c31126484faa061
Ariel2107 <ariel@lawebox.com>
2022-05-17 23:14:19 +0200

    C++'s Advances

---
+++ Library C++/utils_ariel.h
@@ -0,0 +1,12 @@
+//
+// Created by ariel on 16/05/2022.
+//
+
+#ifndef PROJET_ANNUEL_UTILS_H
+#define PROJET_ANNUEL_UTILS_H
+
+double tanh_me(double z);
+
+int sum(double* W, float* inputs);
+
+#endif //PROJET_ANNUEL_UTILS_H
