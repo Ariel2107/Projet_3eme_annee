cbcdffedbe2228c60f11cfd4fa4cd8825527390d
msarambounou <msarambounou11@gmail.com>
2022-05-19 22:57:16 +0200

    Predict linear model

---
+++ Library C++/tools_mamadou.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mamadou SARAMBOUNOU on 19/05/2022.
+//
+
+#include "tools_mamadou.h"
---
+++ Library C++/tools_mamadou.h
@@ -0,0 +1,16 @@
+//
+// Created by Mamadou SARAMBOUNOU on 19/05/2022.
+//
+
+#ifndef PROJET_ANNUEL_TOOLS_MAMADOU_H
+#define PROJET_ANNUEL_TOOLS_MAMADOU_H
+
+
+
+class tools_mamadou {
+
+};
+
+
+
+#endif //PROJET_ANNUEL_TOOLS_MAMADOU_H
